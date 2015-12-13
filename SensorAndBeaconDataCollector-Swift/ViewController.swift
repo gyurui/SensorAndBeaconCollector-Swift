ebffd11cffb63fe7fa6971b9317a0f7b0382c238
gyurui <gyuri.trum@gmail.com>
2015-12-14 00:15:29 +0100

    Display accelerometer data and stop updates button

--- SensorAndBeaconDataCollector-Swift/ViewController.swift
+++ SensorAndBeaconDataCollector-Swift/ViewController.swift
@@ -7,19 +7,49 @@
 //
 
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var accelerationXUILabel: UILabel!
+    @IBOutlet weak var accelerationYUILabel: UILabel!
+    @IBOutlet weak var accelerationZUILabel: UILabel!
+    
+    @IBOutlet weak var btnStopAccelerometerUIButton: UIButton!
+    
+    var cbCentralManager : CMMotionManager = CMMotionManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+        cbCentralManager.accelerometerUpdateInterval = 0.05
 
+        
+        NSTimer.scheduledTimerWithTimeInterval(0.1, target: self, selector: "getValues", userInfo: nil, repeats: true)
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        cbCentralManager.startAccelerometerUpdates()
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        cbCentralManager.stopAccelerometerUpdates()
+    }
+    
+    @IBAction func pushStopAccelerometerButton(sender: AnyObject) {
+        cbCentralManager.stopAccelerometerUpdates()
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func getValues()
+    {
+        accelerationXUILabel.text = "X:\(round(<#T##Double#>)(cbCentralManager.accelerometerData?.acceleration.x)!)"
+        accelerationYUILabel.text = "Y:\((cbCentralManager.accelerometerData?.acceleration.y)!)"
+        accelerationZUILabel.text = "Z:\((cbCentralManager.accelerometerData?.acceleration.z)!)"
+    }
 
 }
 
